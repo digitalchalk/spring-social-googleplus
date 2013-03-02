135ab15acade70678fd83f7633859bae2f13a42a
Bob Robinson <bobrob@sprucepine.com>
2013-03-02 13:30:38 -0500

    removed target

--- spring-social-googleplus-webtest/src/main/java/com/digitalchalk/social/googleplus/webtest/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.digitalchalk.social.googleplus.webtest.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ImportResource({ "classpath*:/spring/security-http.xml" })
-public class SecurityConfig {
-
-}
